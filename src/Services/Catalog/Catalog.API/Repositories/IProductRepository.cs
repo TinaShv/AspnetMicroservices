b631e7b2b660638272981a8501255b5a9f84df71
Shirvani.T <tshirvani97@gmail.com>
2023-04-19 11:02:49 +0330

    create product repository

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        //list of products by async mode
+        Task<IEnumerable<Product>> GetProducts();
+        //expecting Id parameter as string as async method
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
