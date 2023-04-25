f82dea7149f555e24f8e97936526e52e5840e379
Shirvani.T <tshirvani97@gmail.com>
2023-04-25 08:51:22 +0330

    make catalog controller and make swagger ready in program.cs add docker compose in project

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -8,7 +8,8 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConncetionString"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            //var client = new MongoClient("mongodb://username:password@ds011111.mongolab.com:11111/db-name");
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,19 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using Catalog.API;
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
+// Add a custom scoped service.
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1");
+        //options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
