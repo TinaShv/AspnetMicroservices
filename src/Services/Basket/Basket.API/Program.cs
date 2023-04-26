b02bdf8d53273768e13fcf234e2d75d8ddce4fb6
Shirvani.T <tshirvani97@gmail.com>
2023-04-26 15:08:09 +0330

    make connection to redis in program.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
+using System.Configuration;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+//builder.Services.AddStackExchangeRedisCache(option => { option.Configuration = "localhost:6379"; });
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    IConfiguration configuration = null;
+    option.Configuration = ConfigurationBinder.GetValue<string>(configuration,"CacheSettings:ConnectionString");
+    //option.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,7 +25,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1");
+        //options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
